e12df74b6a7e930b7990d984aad4e30fc0cdc7f5
Lix <oranzh.cc@gmail.com>
2022-09-30 16:05:22 +0800

    Create incrByPattern.lua

---
+++ incrByPattern.lua
@@ -0,0 +1,17 @@
+local key_prefix = KEYS[1]
+local volume = tonumber(ARGV[1])
+
+-- get all keys matching the prefix
+local keys = redis.call('keys', key_prefix .. '*')
+
+
+if (next(keys) ~= nil) then
+    -- increment each key
+    for i, key in ipairs(keys) do
+        -- if the value of the key less then volume then increment it
+        if (tonumber(redis.call('get', key)) < volume) then
+            redis.call('incr', key)
+        end
+    end
+end
+return redis.call('mget', unpack(keys))
